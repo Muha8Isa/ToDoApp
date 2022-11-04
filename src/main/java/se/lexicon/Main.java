9d0a6babd1ece4bded1d75f3e28140d0ab4c49f8
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2022-11-04 14:14:48 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Person mohammad = new Person(123, "Mohammad", "Alissa","example@hotmail.com");
+        System.out.println(mohammad.getSummary());
+        Person jack = new Person(124, "Jack", "Warack","ex@hotmail.com");
+        System.out.println(jack.getSummary());
+    }
+}
---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,59 @@
+package se.lexicon;
+
+public class Person {
+
+    private static int order = 0;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private static int sequencer = 0;
+
+    public Person (int id, String firstName, String lastName, String email) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setId(id);
+        setEmail(email);
+        this.order = ++sequencer;
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null) throw new IllegalArgumentException("firstName param was null");
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null) throw new IllegalArgumentException("lastName param was null");
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email == null) throw new IllegalArgumentException("email param was null");
+        this.email = email;
+    }
+
+    public String getSummary() {
+        return " Person#" + order +" FirstName: " + firstName + ", LastName: " + lastName + ", ID: " + id + ", Email: " + email;
+    }
+}
