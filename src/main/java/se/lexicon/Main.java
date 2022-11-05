68d6d54ef4bbba4d89119ebfd356d0de4104ce57
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2022-11-05 21:39:05 +0100

    Added TodoItem

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         Person mohammad = new Person(123, "Mohammad", "Alissa","example@hotmail.com");
         System.out.println(mohammad.getSummary());
-        Person jack = new Person(124, "Jack", "Warack","ex@hotmail.com");
+        Person jack = new Person(124, "Jack", "Warack","exempel@gmail.com");
         System.out.println(jack.getSummary());
     }
 }
---
+++ src/main/java/se/lexicon/TodoItem.java
@@ -0,0 +1,90 @@
+package se.lexicon;
+
+import java.time.LocalDate;
+
+
+public class TodoItem {
+    private int id;
+    private String title; // Should not be null
+    private String taskDescription;
+    public LocalDate deadLine = LocalDate.parse("2022 11 06"); // Should not be null;
+    public LocalDate currentDate = LocalDate.now(); // Should not be null
+    private boolean done;
+    private Person creator;
+    private static int order = 0;
+    private static int sequencer = 0;
+
+
+    public void deadLine() {
+        if (currentDate.isAfter(deadLine)) {
+            System.out.println("Deadline has passed");
+        }
+    }
+    public TodoItem(int id, String title, String taskDescription) {
+        setTitle(title);
+        setTaskDescription(taskDescription);
+        setId(id);
+        this.order = ++sequencer;
+    }
+        public int getId () {
+            return id;
+        }
+
+        public void setId ( int id){
+            this.id = id;
+        }
+
+        public String getTitle () {
+            return title;
+        }
+
+        public void setTitle(String title){
+            if (title == null) throw new IllegalArgumentException("title param was null");
+            this.title = title;
+        }
+
+        public String getTaskDescription () {
+            return taskDescription;
+        }
+
+        public void setTaskDescription (String taskDescription){
+            this.taskDescription = taskDescription;
+        }
+
+        public boolean isDone () {
+            return done;
+        }
+
+        public void setDone ( boolean done){
+            this.done = done;
+        }
+
+        public Person getCreator () {
+            return creator;
+        }
+
+        public void setCreator (Person creator){
+            this.creator = creator;
+        }
+
+        public LocalDate getCurrentDate () {
+            return currentDate;
+        }
+
+        public void setCurrentDate (LocalDate currentDate){
+            this.currentDate = currentDate;
+        }
+
+        public LocalDate getDeadLine () {
+            if (deadLine == null) throw new IllegalArgumentException("Deadline param was null");
+            return deadLine;
+        }
+
+        public void setDeadLine (LocalDate deadLine){
+            this.deadLine = deadLine;
+        }
+
+        public String getSummary () {
+            return " Task#" + order + " Title: " + title + ", Task description: " + taskDescription + ", ID: " + id + ", Creator: " + creator +", Deadline: " + deadLine;
+        }
+    }
